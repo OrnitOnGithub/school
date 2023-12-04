f369d9e35f68449f08dc2edf1b332332eaa0a215
Ornit <ornithopter944@gmail.com>
2023-12-04 08:32:57 +0100

    asdf

--- Math-OS/Racines/heron.py
+++ Math-OS/Racines/heron.py
@@ -6,29 +6,67 @@ def heron (A, i) :
     Puis on fait la moyenne de longeur et largeur
     Cette moyenne c'est la nouvelle largeur
     """
+    print(f"x: {A}")
     largeur = A
     longueur = largeur / A
 
-    while (longueur - largeur < 10**(-i)): # Tant que longueur - largeur < 10^-n
+
+    for _ in range(i):
+        print(f"longueur - largeur = {longueur - largeur}")
         longueur = (largeur + longueur) / 2
         largeur = A / longueur
-        print(largeur)
-    
+        print(f"largeur: {largeur} longueur: {longueur}")
+
+        
     return largeur
-
-
-
 
 
 from matplotlib import pyplot as plt
 
+def fonction_racine():
+    x_values = list(range(2, 100))
+    y_values = [heron(x, 100) for x in x_values]
 
-x_values = list(range(1, 100))
+    plt.plot(x_values, y_values)
+    plt.xlabel('x')
+    plt.ylabel('Racine de x')
+    plt.title('Fonction Racine')
+    plt.show()
 
-y_values = [heron(x, 5) for x in x_values]
 
-plt.plot(x_values, y_values)
-plt.xlabel('x')
-plt.ylabel('Square root of x')
-plt.title('Square Root Function')
-plt.show()
+# JSP comment ca marche tout ca
+
+def heron2 (A, i) :
+    """
+    Nombre A
+    Tu fais un rectangle d'aire A
+    Largeur = nombre cherché
+    Puis on fait la moyenne de longeur et largeur
+    Cette moyenne c'est la nouvelle largeur
+    """
+    print(f"x: {A}")
+    largeur = A
+    longueur = largeur / A
+
+
+    for _ in range(i):
+        print(f"longueur - largeur = {longueur - largeur}")
+        longueur = (largeur + longueur) / 2
+        largeur = A / longueur
+        print(f"largeur: {largeur} longueur: {longueur}")
+
+        
+    return longueur - largeur
+
+def précision(a):
+    x_values = list(range(1, 10))
+    y_values = [heron2(a, x) for x in x_values]
+
+    plt.plot(x_values, y_values)
+    plt.xlabel('x')
+    plt.ylabel('Racine de x')
+    plt.title('Fonction Racine')
+    plt.show()
+
+
+précision(13)
---
+++ Physics-OS/Mouvement/revisions.md
@@ -0,0 +1,17 @@
+# Test mouvement, centre de masse...
+
+## Champ de révision
+
+```
+Test :
+-> Comprendre la notion de centre de masse et l'appliquer dans un problème mécanique.
+
+-> Différentier les types de collisions.
+
+-> Utiliser la conservation de quantité de mouvement pour résoudre un problème de mécanique en 1d.
+
+-> Utiliser la conservation de quantité de mouvement pour résoudre un problème de mécanique en 2d.
+
+->  Comprendre et utiliser la notion d'impulsion
+```
+
